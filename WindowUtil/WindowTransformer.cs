358aa50c591736718aba2e4e716ebb5bb5a481ad
Unknown <tom@dlnet.us>
2018-05-26 04:19:50 +0200

    fix

--- WindowUtil/WindowTransformer.cs
+++ WindowUtil/WindowTransformer.cs
@@ -6,104 +6,28 @@ namespace WindowUtil
 {
     public class WindowTransformer
     {
-        public static void MoveBehindIcons(IntPtr windowHandle)
+         public static void MoveBehindIcons(IntPtr windowHandle)
         {
-            //PrintVisibleWindowHandles(2);
-            // The output will look something like this. 
-            // .....
-            // 0x00010190 "" WorkerW
-            //   ...
-            //   0x000100EE "" SHELLDLL_DefView
-            //     0x000100F0 "FolderView" SysListView32
-            // 0x000100EC "Program Manager" Progman
-
-
-
-            // Fetch the Progman window
-            IntPtr progman = W32.FindWindow("Progman", null);
-
+            IntPtr window = W32.FindWindow("Progman", (string)null);
             IntPtr result = IntPtr.Zero;
-
-            // Send 0x052C to Progman. This message directs Progman to spawn a 
-            // WorkerW behind the desktop icons. If it is already there, nothing 
-            // happens.
-            W32.SendMessageTimeout(progman,
-                                   0x052C,
-                                   new IntPtr(0),
-                                   IntPtr.Zero,
-                                   W32.SendMessageTimeoutFlags.SMTO_NORMAL,
-                                   1000,
-                                   out result);
-
-
-            //PrintVisibleWindowHandles(2);
-            // The output will look something like this
-            // .....
-            // 0x00010190 "" WorkerW
-            //   ...
-            //   0x000100EE "" SHELLDLL_DefView
-            //     0x000100F0 "FolderView" SysListView32
-            // 0x00100B8A "" WorkerW                                   <--- This is the WorkerW instance we are after!
-            // 0x000100EC "Program Manager" Progman
-
+            int num1 = 1324;
+            IntPtr wParam = new IntPtr(0);
+            IntPtr zero = IntPtr.Zero;
+            int num2 = 0;
+            int num3 = 1000;
+            W32.SendMessageTimeout(window, (uint)num1, wParam, zero, (W32.SendMessageTimeoutFlags)num2, (uint)num3, out result);
             IntPtr workerw = IntPtr.Zero;
-
-            // We enumerate all Windows, until we find one, that has the SHELLDLL_DefView 
-            // as a child. 
-            // If we found that window, we take its next sibling and assign it to workerw.
-            W32.EnumWindows((tophandle, topparamhandle) =>
+            W32.EnumWindows((W32.EnumWindowsProc)((tophandle, topparamhandle) =>
             {
-                IntPtr p = W32.FindWindowEx(tophandle,
-                                            IntPtr.Zero,
-                                            "SHELLDLL_DefView",
-                                            IntPtr.Zero);
-
-                if (p != IntPtr.Zero)
-                {
-                    // Gets the WorkerW Window after the current one.
-                    workerw = W32.FindWindowEx(IntPtr.Zero,
-                                               tophandle,
-                                               "WorkerW",
-                                               IntPtr.Zero);
-                }
-
+                if (W32.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", IntPtr.Zero) != IntPtr.Zero)
+                    workerw = W32.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", IntPtr.Zero);
                 return true;
-            }, IntPtr.Zero);
+            }), IntPtr.Zero);
 
-            // We now have the handle of the WorkerW behind the desktop icons.
-            // We can use it to create a directx device to render 3d output to it, 
-            // we can use the System.Drawing classes to directly draw onto it, 
-            // and of course we can set it as the parent of a windows form.
-            //
-            // There is only one restriction. The window behind the desktop icons does
-            // NOT receive any user input. So if you want to capture mouse movement, 
-            // it has to be done the LowLevel way (WH_MOUSE_LL, WH_KEYBOARD_LL).
+            IntPtr dc = W32.GetDCEx(workerw, IntPtr.Zero, (W32.DeviceContextValues)0x403);
+            W32.ReleaseDC(workerw, dc);
 
-
-            // Demo 1: Draw graphics between icons and wallpaper
-
-
-            // Get the Device Context of the WorkerW
-            //IntPtr dc = W32.GetDCEx(workerw, IntPtr.Zero, (W32.DeviceContextValues)0x403);
-            //if (dc != IntPtr.Zero)
-            //{
-                // Create a Graphics instance from the Device Context
-               // using (Graphics g = Graphics.FromHdc(dc))
-                //{
-
-                    // Use the Graphics instance to draw a white rectangle in the upper 
-                    // left corner. In case you have more than one monitor think of the 
-                    // drawing area as a rectangle that spans across all monitors, and 
-                    // the 0,0 coordinate beeing in the upper left corner.
-                    //g.FillRectangle(new SolidBrush(Color.White), Screen.PrimaryScreen.Bounds);
-                //}
-                // make sure to release the device context after use.
-               // W32.ReleaseDC(workerw, dc);
-          //  }
-
-           // W32.SetParent(windowHandle, workerw);
-
-            //return dc;
+            W32.SetParent(windowHandle, workerw);
         }
     }
 
